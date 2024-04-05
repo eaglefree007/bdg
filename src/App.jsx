3a8358952145bfab5a122c35a1b0ba139c573206
surajsah97 <surajsah059@gmail.com>
2024-04-05 11:29:35 -0800

    UI update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import bdg_logo from "./bdg_logo.png";
+import game from "./Assets/game.jpg"
 
 function App() {
   const [period, setPeriod] = useState(""); // Initialize state with empty string
@@ -29,89 +30,78 @@ function App() {
   // console.log(bg)
 
   return (
-    <div className="w-100 sm:w-[688px] relative  h-[100vh] px-3 py-1  flex flex-col justify-around">
-      <div className=" w-11/12 mx-auto flex flex-col sm:flex-row gap-6 justify-around items-center">
-        <div className="w-full mx-auto  p-1">
+    <div className=" bg-[#000000f5] w-100 sm:w-[688px] relative  h-[100vh] px-3  flex flex-col justify-around">
+      
+      <div className=" w-11/12 mx-auto flex flex-col justify-around items-center">
+        <div className="w-8/12 mx-auto  p-1">
           <img src={bdg_logo} alt="" className="w-full h-auto" />
         </div>
-        <div className="font-bold mx-auto ">
-          <div className="for_img">
-            {/* <h2 className="font-xl mx-auto font-bold">BDG Game</h2> */}
-            {/* <img src="" alt="" /> */}
-            <p className=" text-center text-xl">
-              Welcome to our app BDG Game. Enter your period Number and select
-              result type and see the result.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="w-11/12 mx-auto gap-6 my-2">
-        <hr className="border-white-full border" />
-        <p className="font-bold text-center text-2xl w-full mx-auto my-3">
-          BDG Game Mod
-        </p>
-        <hr className="border-white-full border" />
+        <span className="font-bold text-[6rem] mt-[-3rem] mb-[-1rem] w-[100vw] text-yellow-200 text-center my-3">
+          H@ck
+        </span>
+        <hr className="border-white-full border w-1/2 mx-auto" />
       </div>
 
-      <div className=" my-2 flex flex-col justify-between items-center w-[80%] sm:w-[70%] mx-auto">
-        <div className="flex justify-between my-auto w-[63%] mx-auto">
-          <div className="flex ml-2 mr-4 items-center">
-            <p className="">🥳</p>
-            <p className=" mx-2 font-bold text-3xl">RESULT</p>
+      <div className=" my-2 flex flex-col justify-between  items-center w-[90%] sm:w-[80%] mx-auto">
+      
+        <div className="flex justify-between items-center rounded-xl px-4 py-2 mx-auto bg-green-200 mb-2 text-green-900 text-2xl">
+          <div className="flex items-center">
+            <p className="font-bold ">Server: </p>
           </div>
-          <div
-            className={`mx-auto text-3xl h-8 pr-4  rounded-md py-auto font-bold`}
-          >
-            {period && color == "green" ? "🟢" : "🔴"}
+          <div className={`mx-auto rounded-md font-bold`}>
+            {period && color == "green" ? " ~ Connected_[USA]" : "🔴"}
           </div>
         </div>
 
-        <div className=" flex justify-between items-center font-bold w-auto p-2 mx-auto">
-          <p className="mx-6 text-xl">SERVER </p>
-          {/* <span className="">:</span> */}
-          <p className="border w-[65%] border-pink-50  px-4  rounded-md py-auto py-2 my-2 font-bold bg-green-900">
-            CONNECTED
-          </p>
-        </div>
-
-        <div className="w-[67%] mx-auto flex font-bold justify-between items-center mb-4">
-          <div className="flex mx-4 items-center">
-            <p className="">⌚ </p>
-            <p className=" text-xl">PERIOD </p>
-          </div>
+        <div className=" flex font-bold justify-between text-xl px-4 bg-pink-300 rounded-xl py-2 items-center mb-2">
+          <p className=" text-pink-900">WinGo 1Minute Period: </p>
           <input
             type="number"
-            min="100"
+            minLength={3}
             maxLength={3}
-            className="w-[80%] p-1 rounded-md border border-white text-black"
+            className="p-1 w-1/5 rounded-md border border-pink-900 bg-pink-300 text-black"
             value={period} // Set input value from state
             onChange={handleInputChange} // Call handler on change
           />
         </div>
+
+        <div className=" flex font-bold justify-between mx-4 px-4 bg-white/50 rounded-xl py-2 items-center mb-4">
+          <div className=" text-2xl text-purple-800 flex justify-center w-100 items-center ">
+            <p className="px-1">❤️‍🔥</p>
+            <p className="px-1">Result</p>
+            <p className="px-1">❤️‍🔥</p>
+            <p className="px-1">:</p>
+          </div>
+          {/* <span className="">:</span> */}
+          <p className="w-[80%]  px-4 rounded-md font-extrabold text-3xl">
+            {period && color == "green" ? "🟢" : "🔴"}
+          </p>
+        </div>
       </div>
+
+      <div className=" w-1/2 mx-auto">
+        <img src={game} alt="" className="" />
+      </div>
+
       <button
         onClick={changeColor}
-        className="flex justify-center items-center mx-auto bg-gray-700 text-white"
+        className="flex justify-center items-center mx-auto text-gray-700 mb-2 bg-white/90 text-xl "
       >
-        Start hack
+        H@CK 
       </button>
-      {/* <div className="hidden sm:block w-1/2 mx-auto">
-        <img src={bdg_logo} alt="" className="" />
-      </div> */}
       <div className="text-center">
-        <p className="font-bold mb-6">Join us on telegram and download</p>
         <div className="flex justify-around items-center mb-6">
           <a
             href="https://telegram.dog/onlineluterz"
-            className="hover:border-gray-300 hover:bg-blue-400 hover:text-white border border-transparent transition duration-300 ease-in-out px-4 py-2 rounded-md text-2xl"
+            className="hover:border-gray-300 hover:bg-blue-400 bg-yellow-200 hover:text-white border border-transparent transition duration-300 ease-in-out px-4 py-2 rounded-md text-2xl"
           >
-            TeleGram
+            Connect
           </a>
           <a
             href="https://bdggame.in/#/register?invitationCode=671443560283"
-            className="hover:border-gray-300 hover:bg-green-400 hover:text-white border border-transparent transition duration-300 ease-in-out px-4 py-2 rounded-md text-2xl"
+            className="hover:border-gray-300 hover:bg-green-400 hover:text-white border border-transparent transition duration-300 ease-in-out uppercase px-4 py-2 rounded-md text-2xl"
           >
-            ReGister
+            Join
           </a>
         </div>
       </div>
