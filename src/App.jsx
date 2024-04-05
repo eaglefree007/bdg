4cd8950cb8ded02349d9657e065790ed57c46dad
eaglefree007 <75333835+eaglefree007@users.noreply.github.com>
2024-04-05 19:48:09 +0000

    UI Changed

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,6 @@ function App() {
         <div className="w-8/12 mx-auto  p-1">
           <img src={bdg_logo} alt="" className="w-full h-auto" />
         </div>
-<<<<<<< HEAD
         <span className="font-bold text-[6rem] mt-[-3rem] mb-[-1rem] w-[100vw] text-yellow-200 text-center my-3">
           H@ck
         </span>
@@ -61,51 +60,6 @@ function App() {
             minLength={3}
             maxLength={3}
             className="p-1 w-1/5 rounded-md border border-pink-900 bg-pink-300 text-black"
-=======
-        <div className="font-bold mx-auto ">
-          <div className="for_img">
-            {/* <h2 className="font-xl mx-auto font-bold">BDG Game</h2> */}
-            {/* <img src="" alt="" /> */}
-            <p className=" text-center text-xl">
-              ind
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="w-11/12 mx-auto gap-6 my-2">
-        <hr className="border-white-full border" />
-        <p className="font-bold text-center text-2xl w-full mx-auto my-3">
-          onlineluter h@ck
-        </p>
-        <hr className="border-white-full border" />
-      </div>
-
-      <div className=" my-2 flex flex-col justify-around w-[80%] sm:w-[70%] mx-auto">
-
-        <div className="flex justify-between my-auto w-1/2 mx-auto">
-          <p className=" font-bold text-3xl">RESULT</p>
-          <div
-            className={`mx-auto text-3xl h-8 px-4  rounded-md py-auto font-bold`}
-          >
-            {color == "green" ? "🟢" : "🔴"}
-          </div>
-        </div>
-
-        <div className=" flex justify-between items-center font-bold w-auto p-2 mx-auto">
-          <p className="mx-6 text-xl">SERVER</p>
-          {/* <span className="">:</span> */}
-          <p className="border border-pink-50  px-4  rounded-md py-auto py-2 my-2 font-bold bg-green-900">ind~CONECTED
-          </p>
-        </div>
-
-        <div className="w-1/2 mx-auto flex font-bold justify-between items-center mb-4">
-          <p className="">~PERIOD!</p>
-          <input
-            type="number"
-            min="100"
-            maxLength={4}
-            className="w-1/3 p-1 rounded-md border border-white text-black"
->>>>>>> 60f31f1696148416e77de5383018d135957f53bf
             value={period} // Set input value from state
             onChange={handleInputChange} // Call handler on change
           />
@@ -133,22 +87,21 @@ function App() {
         onClick={changeColor}
         className="flex justify-center items-center mx-auto text-gray-700 mb-2 bg-white/90 text-xl "
       >
-        Start hack
+        H@CK 
       </button>
       <div className="text-center">
-        <p className="font-bold mb-6">Join us on telegram and download</p>
         <div className="flex justify-around items-center mb-6">
           <a
             href="https://telegram.dog/onlineluterz"
-            className="hover:border-black-300 hover:bg-orange-400 hover:text-white border border-transparent transition duration-300 ease-in-out px-4 py-2 rounded-md text-2xl"
+            className="hover:border-gray-300 hover:bg-blue-400 bg-yellow-200 hover:text-white border border-transparent transition duration-300 ease-in-out px-4 py-2 rounded-md text-2xl"
           >
-            TeleGram
+            Connect
           </a>
           <a
             href="https://bdggame.in/#/register?invitationCode=671443560283"
             className="hover:border-gray-300 hover:bg-green-400 hover:text-white border border-transparent transition duration-300 ease-in-out uppercase px-4 py-2 rounded-md text-2xl"
           >
-            ReGister
+            Join
           </a>
         </div>
       </div>
